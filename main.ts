48c4887801af822cb07e26174c607fb054362b1b
Fatona Ayomide <ayomidefemi780@gmail.com>
2024-12-23 01:45:00 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Fatona Ayomide",100)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Is a Man",100)
+})
+basic.showString("Hello!")
